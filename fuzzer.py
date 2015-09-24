2f2cce5f03344d6c4721c20988acb91ef98e2fc5
Ajin Abraham <ajin25@gmail.com>
2015-09-24 16:08:39 +0530

    XSS Fuzzer

--- fuzzer.py
+++ fuzzer.py
@@ -2,17 +2,17 @@
 from selenium import webdriver
 from threading import Thread
 from datetime import datetime
-import signal,sys,time,base64
+import signal,sys,time,base64,urllib2
 import tornado.ioloop,tornado.web,tornado.options
 
-
 SERVER_PORT=8787
-BASE_URL='http://127.0.0.1:9999/?xss='
+#BASE_URL='http://localhost:10080/xss/inline?injected_code='
+BASE_URL='http://localhost:9999/?xss='
 PAYLOADS='payload.dat'
 DELAY=3
 
-#Logger
 
+#Logger
 def LOG(data):
 	with open('logs.txt','a') as f:
 		f.write(data +"\n")
@@ -42,26 +42,41 @@ def try_exit():
         except:
         	pass
         tornado.ioloop.IOLoop.instance().stop()
+
+#Server Reachable
+def Ping(NO,PAYLOAD,BASE_URL):
+	try:
+		urllib2.urlopen(BASE_URL)
+	except urllib2.HTTPError, e:
+		print(e)
+		pass
+	except urllib2.URLError, e:
+		DAT = "\n\n[ERROR] Server is not Reachable, Last tried [PAYLOAD]: " + str(NO) +". "+ PAYLOAD+" [URL]: "+BASE_URL+" Error: "+ str(e.args)
+		print DAT
+		LOG(DAT)
+		sys.exit(0)
+
 #Web Driver
 
 def DriverThread(DRIVER,BASE_URL,PAYLOADS,DELAY):
 	with open(PAYLOADS,'r') as f:
 		payload_collection=f.readlines()
-	print "Started Fuzzing\n Press Ctrl + C to Quit"
+	print "Started Fuzzing\nPress Ctrl + C to Quit"
 	count=0
 	for payload in payload_collection:
 		count+=1
 		DAT="No: "+ str(count) +" Data: " +str(payload)
 		LOG(DAT)
-		#print DAT
 		try:
 			DRIVER.get(BASE_URL+payload)
-			time.sleep(DELAY)
+	    		Ping(count,payload,BASE_URL)
+	    		time.sleep(DELAY)
 		except Exception as e:
-			print "[ERROR] Selenium - " + str(e)
+			#print "[ERROR] Selenium - " + str(e)
 			pass
 	print "Fuzzing Completed"
 	DRIVER.quit()
+	sys.exit(0)
 
 thread = Thread(target=DriverThread,args=[DRIVER,BASE_URL,PAYLOADS,DELAY])
 thread.setDaemon(True)
